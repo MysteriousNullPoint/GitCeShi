1df5074e3d9d570e447d30cf3786f115a496ec88
wangzhao <13011114585@163.com>
2021-02-02 16:47:17 +0800

    dev提交

--- app/src/main/java/com/ele/gitceshi/MainActivity.java
+++ app/src/main/java/com/ele/gitceshi/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private  String testT = "dev提交";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
