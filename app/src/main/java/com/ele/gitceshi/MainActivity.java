83d21230a68c25401635ebec049a076690441880
“wangzhao” <133011114585@163.com>
2021-03-25 17:57:28 +0800

    dev3

--- app/src/main/java/com/ele/gitceshi/MainActivity.java
+++ app/src/main/java/com/ele/gitceshi/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     private  String testT = "dev提交";
     private  String testT2 = "de2提交";
     private  String testT3 = "dev2提交测试文版";
+    private  String testT4 = "dev3";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
